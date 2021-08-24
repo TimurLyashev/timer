4c758eda87c3d0e905419b0fe17a5d9da57a8442
Timur L'yashev <timur.lyashev@gmail.com>
2021-08-24 23:15:51 +0300

    Coding style

--- src/timer.h
+++ src/timer.h
@@ -16,6 +16,16 @@ struct expand_type {
 
 template <typename Callable, typename... Args>
 class Timer {
+    std::unique_ptr<std::thread> loop_thread;
+
+    const std::chrono::milliseconds tick;
+
+    std::function<Callable> callable_function;
+    std::tuple<Args...> callable_function_args;
+
+    std::atomic_flag start_flag = ATOMIC_FLAG_INIT;
+    std::atomic_flag stop_flag = ATOMIC_FLAG_INIT;
+
     void loop()
     {
         while (!start_flag.test_and_set(std::memory_order_acquire)) {
@@ -31,17 +41,18 @@ class Timer {
         }
     }
 
-    std::unique_ptr<std::thread> loop_thread;
-
-    const std::chrono::milliseconds tick;
-
-    std::function<Callable> callable_function;
-    std::tuple<Args...> callable_function_args;
-
-    std::atomic_flag start_flag = ATOMIC_FLAG_INIT;
-    std::atomic_flag stop_flag = ATOMIC_FLAG_INIT;
-
 public:
+    explicit Timer(int new_tick, Callable&& __f, Args&&... __args)
+        : callable_function(__f)
+        , callable_function_args(__args...)
+        , tick(new_tick)
+    {
+#ifdef DEBUG
+        std::cout << "timer func: " << (int*)__f << "\n";
+        expand_type { 0, (std::cout << __args << "\n", 0)... };
+#endif
+    }
+
     void create()
     {
         loop_thread = std::make_unique<std::thread>(&Timer::loop, this);
@@ -58,16 +69,6 @@ public:
         stop_flag.test_and_set(std::memory_order_acquire);
     }
 
-    explicit Timer(int new_tick, Callable&& __f, Args&&... __args)
-        : callable_function(__f)
-        , callable_function_args(__args...)
-        , tick(new_tick)
-    {
-#ifdef DEBUG
-        std::cout << "timer func: " << (int*)__f << "\n";
-        expand_type { 0, (std::cout << __args << "\n", 0)... };
-#endif
-    }
 };
 
 #endif // TIMER_H
