c8725afa0882c26de50f572611e74c6c9686379b
Timur L'yashev <t.liashev@kamov.ru>
2021-08-24 10:47:21 +0300

    Tests are separated

--- tests/main.cpp
+++ tests/main.cpp
@@ -11,14 +11,16 @@ void test_message_counter(int& counter)
 int main()
 {
     int counter = 0;
-    int timer_tick = 100;
+    int timer_tick = 100; //milliseconds
+    int pause = 10; //seconds
+
     std::cout << "programm func: " << (int*)test_message_counter << "\n";
     Timer<void(int&), int&> test_timer(timer_tick, test_message_counter, counter);
 
     test_timer.create();
     test_timer.start();
 
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(pause));
 
     test_timer.stop();
 
