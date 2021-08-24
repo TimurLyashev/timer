7c8fcdba35150d64e1d10c196c0c3e2c0e2a3882
Льяшев Тимур Равшанович <t.liashev@kamov.ru>
2021-08-24 08:54:20 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "timer.h"
+
+#include <iostream>
+
+void test_message_counter(int& counter)
+{
+    counter++;
+    std::cout << counter << "\n";
+}
+
+int main()
+{
+    int counter = 0;
+    int timer_tick = 100;
+    std::cout << "programm func: " << (int*)test_message_counter << "\n";
+    Timer<void(int&),int&> test_timer(timer_tick, test_message_counter, counter);
+
+    test_timer.create();
+    test_timer.start();
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    test_timer.stop();
+
+    return 0;
+}
---
+++ timer.h
@@ -0,0 +1,75 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <thread>
+#include <functional>
+#include <atomic>
+
+#ifdef DEBUG
+#include <iostream>
+
+struct expand_type {
+    template<typename... T>
+    expand_type(T&&...) {}
+};
+#endif
+
+template<typename _Callable, typename... _Args>
+class Timer {
+    void loop()
+    {
+        while (!start_flag.test_and_set(std::memory_order_acquire)) {
+            start_flag.clear(std::memory_order_release);
+        }
+
+        start_flag.test_and_set(std::memory_order_acquire);
+
+        while (!stop_flag.test_and_set(std::memory_order_acquire)) {
+            std::apply(callable_function, callable_function_args);
+            std::this_thread::sleep_for(tick.load());
+            stop_flag.clear(std::memory_order_release);
+        }
+    }
+
+    std::unique_ptr<std::thread> loop_thread;
+
+    std::atomic<std::chrono::milliseconds> tick = std::chrono::milliseconds(1);
+
+    std::function<_Callable> callable_function;
+    std::tuple<_Args...> callable_function_args;
+
+    std::atomic_flag start_flag = ATOMIC_FLAG_INIT;
+    std::atomic_flag stop_flag = ATOMIC_FLAG_INIT;
+
+public:
+    void create()
+    {
+        loop_thread = std::make_unique<std::thread>(&Timer::loop, this);
+        loop_thread->detach();
+    }
+    void start()
+    {
+        start_flag.test_and_set(std::memory_order_acquire);
+    }
+    void stop()
+    {
+        stop_flag.test_and_set(std::memory_order_acquire);
+    }
+
+    explicit Timer(int new_tick, _Callable&& __f, _Args&&... __args)
+        :
+        callable_function(__f),
+        callable_function_args(__args...)
+    {
+        tick.store(std::chrono::milliseconds(new_tick));
+#ifdef DEBUG
+        std::cout << "timer func: " << (int*)__f << "\n";
+        expand_type{ 0, (std::cout << __args << "\n", 0)... };
+#endif
+    }
+
+    void set_tick(int new_tick);
+    std::chrono::milliseconds get_tick();
+};
+
+#endif // TIMER_H
